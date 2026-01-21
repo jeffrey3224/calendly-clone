15976c07dbdfe24c9e2f8f99a4e57fece1e373b7
Jeffrey Girard <jeffrey3224@gmail.com>
2026-01-21 11:29:59 -0800

    updated ui on landing page

--- components/LandingPage.tsx
+++ components/LandingPage.tsx
@@ -6,33 +6,46 @@ import Image from "next/image"
 
 export default function LandingPage() {
   return (
-    <main className="flex items-center justify-center p-10 gap-24 animate-fade-in max-md:flex-col">
-      <section className="flex flex-col items-center">
-        <Image src="/assets/logo.svg"
-        width={300}
-        height={300}
-        alt="logo"
-        />
+    <main className="flex flex-col min-h-screen p-5 sm:px-15 bg-gradient-to-r from-blue-100 to-blue-300 animate-fade-in">
+  {/* Logo at the top */}
+  <div className="flex justify-center pt-5">
+    <Image
+      src="/assets/logo-alternate.svg"
+      width={250}
+      height={20}
+      alt="logo"
+      className="w-30 sm:w-40 md:w-60 lg:w-70"
+    />
+  </div>
 
-        <h1 className="text-2xl font-black lg:text-3xl">
-          Your time, perfectly planned
-        </h1>
-        <p className="font-extralight">
-          Join millions of professionals who easily book meetings with the #1 scheduling tool
-        </p>
-        <Image src="/assets/planning.svg"
+  {/* Centered content below logo */}
+  <div className="flex flex-col lg:flex-row items-center justify-center flex-1 py-10 lg:gap-24 max-lg:flex-col">
+    <div className="order-2 lg:order-1">
+      <SignIn
+        routing="hash"
+        appearance={{ baseTheme: neobrutalism }}
+      />
+    </div>
+
+    <section className="flex flex-col items-center order-1 lg:order-2 pb-7 lg:pb-0">
+      <h1 className="text-3xl font-black lg:text-4xl pb-2">
+        Your time, perfectly planned
+      </h1>
+      <p className="text-[1.1rem] font-bold text-gray-700 max-w-[500px]">
+        Join millions of professionals who easily book meetings with the #1 scheduling tool
+      </p>
+
+      <Image
+        src="/assets/planning.svg"
         width={500}
         height={500}
-        alt="logo"/>
-      </section>
+        alt="illustration"
+        role="presentation"
+        className="w-60 lg:w-120"
+      />
+    </section>
+  </div>
+</main>
 
-      <div className="mt-3">
-        <SignIn
-          routing="hash"
-          appearance={{
-            baseTheme: neobrutalism
-          }}/>
-      </div>
-    </main>
   )
 }
